a2760cc3fd2be1a02f794b4e98105cb6fffe4213
shaunkatona <shaun.katona@gmail.com>
2014-07-27 18:48:16 -0600

    coaches dropdown so you can add to other teams

--- README.md
+++ README.md
@@ -6,8 +6,9 @@ Mostly just an exercise in AngularJS, but it's a fantasy football draft tool.
 Todo
 ===
 - styling
-  - color code positions
 - local storage?
 - find $ values?
 - track other teams or just yourself and available?
 - enforce/recommend positions (must have 1 QB, 1 K, 2 RBs, etc)
+- sort by any column
+    - back to default sorting (position then positionrank)
--- public/scripts/app.js
+++ public/scripts/app.js
@@ -7,10 +7,23 @@
     app.controller('PlayerController', ['$scope', '$http', function ($scope, $http) {
         $scope.players = {
             available: [],
-            taken: {
-                mine: []
+            mine: [],
+            theirs: {
+                "Steve Racz": [],
+                Jarrod: [],
+                Dale: [],
+                Ryan: [],
+                Brandt: [],
+                "Steve Okell": [],
+                Renee: [],
+                Chris: []
             }
         };
+
+        // TODO: add/edit/remove coaches?
+        $scope.coaches = [
+            "Steve Racz", "Jarrod", "Dale", "Ryan", "Brandt", "Steve Okell", "Renee", "Chris"
+        ];
 
         $http.get('json/rankings_converted.json').then(function (res) {
             $scope.players.available = res.data;
@@ -19,15 +32,28 @@
         // take the player out of the available queue and add them to our queue
         $scope.addPlayer = function (player) {
             $scope.players.available.splice($scope.players.available.indexOf(player), 1);
-            $scope.players.taken.mine.push(player);
+            $scope.players.mine.push(player);
+        };
+
+        // take the player out of the available queue and add them to another coach's team (not mine)
+        $scope.addPlayerToTheirs = function (player, coach) {
+            $scope.players.available.splice($scope.players.available.indexOf(player), 1);
+            $scope.players.theirs[coach].push(player);
         };
 
         // take the player out of our queue and add them to the available queue
         $scope.releasePlayer = function (player) {
-          $scope.players.taken.mine.splice($scope.players.taken.mine.indexOf(player), 1);
+          $scope.players.mine.splice($scope.players.mine.indexOf(player), 1);
           $scope.players.available.push(player);
         };
 
+        // take the player out of their queue and add them to the available queue
+        $scope.releasePlayerFromTheirs = function (player, coach) {
+            $scope.players.theirs[coach].splice($scope.players.theirs[coach].indexOf(player), 1);
+            $scope.players.available.push(player);
+        };
+
+        // to color code the rows by position
         $scope.getRowClass = function (player) {
           return player.position;
         };
